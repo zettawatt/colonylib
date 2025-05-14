1dc7e7c64b0a4b09eb793b10515146bce4e87bc2
Chuck McClish <github.vanish213@passmail.net>
2025-05-13 23:14:39 -0400

    Implemented add_pod method. The code is ugly right now, but it worksgs

--- src/network.rs
+++ src/network.rs
@@ -1,12 +1,16 @@
-use autonomi::{Client, Wallet, SecretKey, PublicKey};
+use autonomi::{Bytes, Client, PublicKey, SecretKey, Signature, Wallet};
+use autonomi::client::pointer::{Pointer, PointerTarget, PointerError, PointerAddress};
+use autonomi::client::scratchpad::{Scratchpad, ScratchpadError, ScratchpadAddress};
+use autonomi::client::payment::PaymentOption;
 use ruint::Uint;
+use hex;
 
 use crate::KeyStore;
 
 #[derive(Clone)]
 pub struct Network {
-    client: Client,
-    wallet: Wallet,
+    pub client: Client,
+    pub wallet: Wallet,
 }
 
 impl Network {
@@ -53,29 +57,95 @@ impl Network {
         Ok(balance)
     }
 
+    // Create a new pointer key, make sure it is empty, and add it to the key store
+    async fn create_pointer_key(&mut self, key_store: &mut KeyStore) -> Result<SecretKey, PointerError> {
+        loop {
+            // Derive a new key
+            let key_string = key_store.add_derived_key();
+            let derived_key: SecretKey = SecretKey::from_hex(key_string.as_str()).unwrap();
+            // Check if the key is empty
+            let address = PointerAddress::new(derived_key.clone().public_key());
+            let already_exists = self.client.pointer_check_existance(&address).await?;
+            if already_exists {
+                println!("Pointer key already exists, generating a new one");
+                continue;
+            } else {
+                return Ok(derived_key);
+            }
+        }
+    }
+
+    // Create a new pointer key, make sure it is empty, and add it to the key store
+    async fn create_scratchpad_key(&mut self, key_store: &mut KeyStore) -> Result<SecretKey, ScratchpadError> {
+        loop {
+            // Derive a new key
+            let key_string = key_store.add_derived_key();
+            let derived_key: SecretKey = SecretKey::from_hex(key_string.as_str()).unwrap();
+            // Check if the key is empty
+            let address = ScratchpadAddress::new(derived_key.clone().public_key());
+            let already_exists = self.client.scratchpad_check_existance(&address).await?;
+            if already_exists {
+                println!("Scratchpad key already exists, generating a new one");
+                continue;
+            } else {
+                return Ok(derived_key);
+            }
+        }
+    }
+
     // Add a new pod
-    pub async fn add_pod(&mut self, mut key_store: KeyStore) -> Result<String, String> {
-        // Derive a new key for the pod pointer
-        let pointer_key = key_store.add_derived_key();
-
-        // Check that this address doesn't contain existing data, else create new key
-
+    pub async fn add_pod(&mut self, key_store: &mut KeyStore) -> Result<(String, String), String> {
         // Derive a new key for the pod scratchpad
-        let scratchpad_key = key_store.add_derived_key();
+        let scratchpad_key: SecretKey = self.create_scratchpad_key(key_store).await.map_err(|e| {
+            println!("Error creating scratchpad key: {e}");
+            format!("Error creating scratchpad key: {e}")
+        })?;
         
-        // Check that this address doesn't contain existing data, else create new key
+        // Create new publicly readable scratchpad
+        let scratchpad_address: ScratchpadAddress = ScratchpadAddress::new(scratchpad_key.clone().public_key());
+        let scratchpad: Scratchpad = Scratchpad::new_with_signature(
+            scratchpad_key.clone().public_key(),
+            0,
+            Bytes::from("It works!"),
+            0,
+            scratchpad_key.sign(Scratchpad::bytes_for_signature(
+                scratchpad_address.clone(),
+                0,
+                &Bytes::from("It works!"),
+                0,
+            )),
+        );
 
-        // Create new scratchpad
+        // Derive a new key for the pod pointer
+        let pointer_key: SecretKey = self.create_pointer_key(key_store).await.map_err(|e| {
+            println!("Error creating pointer key: {e}");
+            format!("Error creating pointer key: {e}")
+        })?;
 
         // Create new pointer that points to the scratchpad
+        let pointer = Pointer::new(
+            &pointer_key,
+            0,
+            PointerTarget::ScratchpadAddress(scratchpad_address),
+        );
 
-        // Get quote to upload pointer and scratchpad
+        //FIXME: batch the pod scratchpad and pointer put operations
+        // Put the scratchpad on the network
+        let payment_option = PaymentOption::from(&self.wallet);
+        let (scratchpad_cost, scratchpad_address) = self.client.scratchpad_put(scratchpad, payment_option.clone()).await.map_err(|e| {
+            println!("Error putting scratchpad on network: {e}");
+            format!("Error putting scratchpad on network: {e}")
+        })?;
+        let (pointer_cost, pointer_address) = self.client.pointer_put(pointer, payment_option).await.map_err(|e| {
+            println!("Error putting pointer on network: {e}");
+            format!("Error putting pointer on network: {e}")
+        })?;
+        println!("Scratchpad address: {scratchpad_address:?}");
+        println!("Scratchpad cost: {scratchpad_cost:?}");
+        println!("Pointer address: {pointer_address:?}");
+        println!("Pointer cost: {pointer_cost:?}");
 
-        // Pay and upload the scratchpad and pointer to the network
-
-        // Update key store with latest data
-
-        Ok(pointer_key) //FIXME: change this to the address of the pointer
+        Ok((pointer_address.to_string(), scratchpad_address.to_string()))
     }
 
     // Update pod
